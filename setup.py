50fe8808ace49b2333438c4d6fa1d4059cdef3da
Your Name <you@example.com>
2021-04-22 08:20:46 +0200

    move all packaging stuff to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 import setuptools
 
-setuptools.setup()
+setuptools.setup(
+    name='omegalul',
+    version='0.0.4',
+    author='pcranaway',
+    author_email='prcanaway@tuta.io',
+    description='A Python library for building omegle clients',
+    url='https://github.com/pcranaway/omegalul',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'}
+)
