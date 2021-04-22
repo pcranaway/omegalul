fb074055023404f81d22b0e40dc9fbdf39b68fa6
Your Name <you@example.com>
2021-04-22 10:24:26 +0200

    bump version again

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('readme.md') as file:
 
 setuptools.setup(
     name='omegalul',
-    version='1.0.0',
+    version='1.0.1',
     author='pcranaway',
     author_email='pcranaway@tuta.io',
     description='A Python library for building omegle clients',
